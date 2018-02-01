d3ba9f2c960674c55a2098ced4b94b9c0081f097
kyao4 <kyao4@hawk.iit.edu>
2018-02-01 15:26:09 +0800

    add api call code and url

---
+++ python_rabbitmq/Tracing_API/Tracing.py
@@ -0,0 +1,12 @@
+'''
+Created on 2018年2月1日
+
+@author: yaokai2
+'''
+
+import urllib.request as urlrequest
+
+if __name__ == '__main__':
+    url_visit = 'http://10.99.206.217:8004/api/trace-files'
+    
+    
