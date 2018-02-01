545fbc00671ff803e59255bb7211cf61fbc4c9cd
kyao4 <kyao4@hawk.iit.edu>
2018-02-01 23:52:00 +0800

    call rabbit tracing api

--- python_rabbitmq/Tracing_API/Tracing.py
+++ python_rabbitmq/Tracing_API/Tracing.py
@@ -4,9 +4,18 @@ Created on 2018年2月1日
 @author: yaokai2
 '''
 
-import urllib.request as urlrequest
+import requests
 
 if __name__ == '__main__':
+    
+    '''
+    GET            /api/traces
+    GET            /api/traces/<vhost>
+    GET PUT DELETE /api/traces/<vhost>/<name>
+    GET            /api/trace-files
+    GET     DELETE /api/trace-files/<name>    (GET returns the file as text/plain)
+    '''
     url_visit = 'http://10.99.206.217:8004/api/trace-files'
-    
-    
+    r = requests.get('http://10.99.206.217:8004/api/traces/vhost_poc', auth=('admin', 'admin'))
+    print(r.status_code)
+    print(r.json())
