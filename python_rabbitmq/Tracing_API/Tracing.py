149a62ccb2c8445de9e2dbc83709c09055edc1d3
kyao4 <kyao4@hawk.iit.edu>
2018-02-02 15:29:36 +0800

    configure connection and use API to add trace

--- python_rabbitmq/Tracing_API/Tracing.py
+++ python_rabbitmq/Tracing_API/Tracing.py
@@ -5,7 +5,7 @@ Created on 2018年2月1日
 '''
 
 import requests
-
+import json
 if __name__ == '__main__':
     
     '''
@@ -16,6 +16,17 @@ if __name__ == '__main__':
     GET     DELETE /api/trace-files/<name>    (GET returns the file as text/plain)
     '''
     url_visit = 'http://10.99.206.217:8004/api/trace-files'
-    r = requests.get('http://10.99.206.217:8004/api/traces/vhost_poc', auth=('admin', 'admin'))
+    r = requests.get('http://10.99.206.217:8004/api/traces', auth=('admin', 'admin'))
+#     print(r.status_code)
+#     print(r.json())
+    
+#     r = requests.put('http://10.99.206.217:8004/api/traces/%2fvhost_poc/test_api_tracing_kai', 
+#                      data= json.dumps({"format":"text",
+#                             "pattern":"deliver.task_queue_kai", 
+#                             "max_payload_bytes":1000}), 
+#                      auth=('admin', 'admin'), 
+#                      headers = {'content-type': 'application/json'})
+#     print(r.status_code)
+#     print(r.json())
+    r = requests.delete('http://10.99.206.217:8004/api/traces/%2fvhost_poc/test_api_tracing_kai', auth=('admin', 'admin'))
     print(r.status_code)
-    print(r.json())
